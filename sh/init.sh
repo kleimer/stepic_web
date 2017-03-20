8af6ebee67998ff7d344099042734d1ce5c3dbf5
Amenitskiy Maxim <m.amenitskiy@gmail.com>
2017-03-20 20:05:43 +0300

    dssd

--- sh/init.sh
+++ sh/init.sh
@@ -4,5 +4,5 @@
 ln -sf ~/web/etc/nginx.cofig /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 mkdir /etc/gunicorn.d
-ln -sf ~/etc/gunicorn-wsgi.conf /etc/gunicorn.d/gunicorn-wsgi.conf
-ln -sf ~/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.conf
+ln -sf ~/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/gunicorn-wsgi.conf
+ln -sf ~/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.conf
